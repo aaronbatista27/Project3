af8e9a7c41a79483c1e96cc88086c1d8a1ef47d0
Aaron Batista <92892460+aaronbatista27@users.noreply.github.com>
2022-12-06 14:58:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,74 +1,145 @@
+#include "Map.h"
+#include "Hash.h"
 #include <iostream>
-#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <chrono>
+
 using namespace std;
+using namespace chrono;
 
-
-class Set
+int main()
 {
-    public:
-        vector<pair<string, int>> arraySet[100];
-        string genre;
-        string name;
+    Map map;
+    Set hash;
+
+    // NOTE: the address of the read-in file must be changed per machine
+    ifstream inFile("C:/Users/Aaron Batista/CLionProjects/MovieProject/newestMovieSet.csv");
+    string line;
+
+    string key;
+    string value1;
+    string value2;
+    int year;
+
+    // initialize mapTime and hashTime with the same value;
+    auto temp1 = high_resolution_clock::now();
+    auto temp2 = high_resolution_clock::now();
+    auto mapTime = duration_cast<microseconds>(temp2 - temp1);
+    auto hashTime = duration_cast<microseconds>(temp2 - temp1);
+
+    //auto mapTime = duration_cast<microseconds>(0);
+
+    while (getline(inFile, line))
+    {
+        istringstream stream(line);
+        // get movie title
+        getline(stream, key, ',');
+
+        // get genres for movie
+        getline(stream, value1, ',');
+
+        getline(stream, value2);
+        year = stoi(value2);
+
+        // time from before and after each of these functions
+        auto startMap = high_resolution_clock::now();
+        map.insert(key, make_pair(value1, year));
+        auto endMap = high_resolution_clock::now();
+        mapTime += duration_cast<microseconds>(endMap - startMap);
+
+        // time from before and after of each of these functions
+        auto startHash = high_resolution_clock::now();
+        hash.insert(value1, key, year);
+        auto endHash = high_resolution_clock::now();
+        hashTime += duration_cast<microseconds>(endHash - startHash);
+
+        key.clear();
+        value1.clear();
+        value2.clear();
+    }
+
+    cout << "Welcome to the Movie Searcher 2000!" << endl;
+    cout << "In this program, you are able to search for movies based on the genres YOU select (ain't that somethin'?)" << endl;
+    cout << "You also have the option of searching for movies by year (only from 1975 onwards)" << endl;
+    cout << endl;
+    cout << "Now go ahead a get movie searching!" << endl;
+    cout << endl;
+    cout << "If you would like to search solely on genres, type 1" << endl;
+    cout << "If you would like to provide a range of years as well, type 2" << endl;
+
+    int choice;
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        string genres;
+
+        // only genre stuff
+        cout << "Great choice!" << endl;
+        cout << "Now, input up to 3 genres! We will recommend the best movies for you based on your input" << endl;
+        cout << "(NOTE: only add one space between genres. For example, action comedy romance)" << endl;
+        cin.ignore();
+        getline(cin, genres);
+
+        cout << "Coming right up!" << endl;
+
+        // map doesn't print anything, still checks for timing purposes
+        auto startMap = high_resolution_clock::now();
+        map.findMovies(genres);
+        auto endMap = high_resolution_clock::now();
+        mapTime += duration_cast<microseconds>(endMap - startMap);
+
+        // hash prints the movies
+        auto startHash = high_resolution_clock::now();
+        hash.search(genres, -1, -1);
+        auto endHash = high_resolution_clock::now();
+        hashTime += duration_cast<microseconds>(endHash - startHash);
+    }
+
+    if (choice == 2)
+    {
+        string genres;
+
+        //genre + year
+        cout << "GREAT choice! Just tell me what years you'd like to search between:" << endl;
         int startYear;
         int endYear;
 
-    public:
-        void insert(string genre, string name, int year);
-        bool search(string genre, string name, int startYear, int endYear);
-};
+        cout << "Starting year: ";
+        cin >> startYear;
+        cout << "End year: ";
+        cin >> endYear;
+        cout << endl;
 
-void Set::insert(string genre, string name, int year)
-{
-    //find the hash of the value
-    //reduce the hash to get an index
-    int asciiValue = 0;
+        cout << "Now, input up to 3 genres! We will recommend the best movies for you based on your input" << endl;
+        cout << "(NOTE: only add one space between genres. For example, action comedy romance)" << endl;
 
-    for(int i = 0; i < genre.size(); i++){
-        int letterToInt = (int)genre[i];
-        asciiValue = asciiValue + letterToInt;
+        cin.ignore();
+        getline(cin, genres);
+
+        cout << "Coming right up!" << endl;
+
+        // map doesn't print anything, still checks for timing purposes
+        auto startMap = high_resolution_clock::now();
+        map.findYears(genres, startYear, endYear);
+        auto endMap = high_resolution_clock::now();
+        mapTime += duration_cast<microseconds>(endMap - startMap);
+
+        // hash prints the movies
+        auto startHash = high_resolution_clock::now();
+        hash.search(genres, startYear, endYear);
+        auto endHash = high_resolution_clock::now();
+        hashTime += duration_cast<microseconds>(endHash - startHash);
+
+        cout << endl;
+        cout << "Hope we helped you pick your next movie!" << endl;
     }
-    asciiValue = asciiValue % 100;
 
-    //insert the name and year of the movie into its corresponding genre
-    if(genre == "\\N"){
-        arraySet[0].push_back(make_pair(name, year));
-    }
-    else {
-        arraySet[asciiValue].push_back(make_pair(name, year));
-    }
+    cout << endl;
+    cout << "Map Time: " << mapTime.count() << " microseconds" << endl;
+    cout << "Hash Time: " << hashTime.count() << " microseconds" << endl;
 
+    return 0;
 }
-
-bool Set::search(string genre, string name, int startYear, int endYear)
-{
-    //find the hash of the value and the corresponding index
-    int hashValue = 0;
-
-    for(int i = 0; i < genre.size(); i++){
-        int letterToInt = (int)genre[i];
-        hashValue = hashValue + letterToInt;
-    }
-    hashValue = hashValue % 100;
-
-    //print out the matching movie title/genre/year of the genre chosen if it is in the range of inputted years
-    if(genre != "\\N") {
-        for (auto itr = arraySet[hashValue].begin(); itr != arraySet[hashValue].end(); itr++) {
-            if (itr->second >= startYear && itr->second <= endYear) {
-                cout << "Name of the movie: " << name << " Genre: " << genre << " Year: " << itr->second << endl;
-            }
-
-        }
-    }
-    if(genre == "\\N"){
-        for (auto itr = arraySet[0].begin(); itr != arraySet[0].end(); itr++) {
-            if (itr->second >= startYear && itr->second <= endYear) {
-                cout << "Name of the movie: " << name << " Genre: " << genre << " Year: " << itr->second << endl;
-            }
-
-        }
-    }
-
-
-}
-
-
