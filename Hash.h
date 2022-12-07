692bad22161fdc85c2cc4978bdb43d14cb57ec30
hannah-dubose <73614102+hannah-dubose@users.noreply.github.com>
2022-12-06 21:24:35 -0500

    Update Hash.h

--- Hash.h
+++ Hash.h
@@ -5,6 +5,7 @@
 using namespace std;
 
 
+//Slides Module 7: Sets, Maps, Hash Tables
 class Set
 {
 private:
@@ -34,51 +35,8 @@ void Set::insert(string genre, string name, int year)
     asciiValue = asciiValue % 1000;
 
     //insert the name and year of the movie into its corresponding genre
-    /*
-    if(genre == "\\N"){
-        arraySet[0].push_back(make_pair(name, year));
-    }
-    else {
-        arraySet[asciiValue].push_back(make_pair(name, year));
-    }
-    */
     hashSet[asciiValue].emplace_back(make_pair(name, year));
 }
-
-/*
-bool Set::search(string genre, string name, int startYear, int endYear)
-{
-    //find the hash of the value and the corresponding index
-    int hashValue = 0;
-
-    for(int i = 0; i < genre.size(); i++){
-        int letterToInt = (int)genre[i];
-        hashValue = hashValue + letterToInt;
-    }
-    hashValue = hashValue % 100;
-
-    //print out the matching movie title/genre/year of the genre chosen if it is in the range of inputted years
-    if(genre != "\\N") {
-        for (auto itr = hashSet[hashValue].begin(); itr != hashSet[hashValue].end(); itr++) {
-            if (itr->second >= startYear && itr->second <= endYear) {
-                cout << "Name of the movie: " << name << " Genre: " << genre << " Year: " << itr->second << endl;
-            }
-
-        }
-    }
-    if(genre == "\\N"){
-        for (auto itr = hashSet[0].begin(); itr != hashSet[0].end(); itr++) {
-            if (itr->second >= startYear && itr->second <= endYear) {
-                cout << "Name of the movie: " << name << " Genre: " << genre << " Year: " << itr->second << endl;
-            }
-
-        }
-    }
-
-
-}
-
-*/
 
 void Set::search(string genre, int startYear, int endYear)
 {
@@ -93,15 +51,7 @@ void Set::search(string genre, int startYear, int endYear)
         }
         hashValue = hashValue % 1000;
 
-        /*
-        //print out the matching movie title/genre/year of the genre chosen if it is in the range of inputted years
-        for (auto itr = hashSet[hashValue].begin(); itr != hashSet[hashValue].end(); itr++) {
-            if (itr->second >= startYear && itr->second <= endYear) {
-                cout << name << " Genre: " << genre << " Year: " << itr->second << endl;
-            }
-
-        }
-         */
+        
         for (int i = 0; i < hashSet[hashValue].size(); ++i)
         {
             cout << hashSet[hashValue].at(i).first << ", " << hashSet[hashValue].at(i).second << endl;
